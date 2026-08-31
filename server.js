3908c400b92a80a7311c76d52daf167f13640337
idealkritarat <ideal141148@gmail.com>
2026-08-31 21:30:41 +0700

    Chapter07

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './config/config.env' });
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.status(200).json({ success: true, data: { id: 1 } });
+})
+
+const hospitals = require(`./routes/hospitals`);
+app.use(`/api/v1/hospitals`, hospitals);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, ' mode on port ', PORT));
